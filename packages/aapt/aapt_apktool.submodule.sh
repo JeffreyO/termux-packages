842e0777a54a73bf0a5b6b78d8db146edd593a06
Jeffrey Ozanne <JeffreyO@users.noreply.github.com>
2023-07-18 19:07:30 -0400

    Create aapt_apktool.submodule.sh
    Testing if making different 'flavors' of the source files works with submodules.

---
+++ packages/aapt/aapt_apktool.submodule.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="bin/aapt_apktool"
+TERMUX_SUBPKG_DESCRIPTION="AAPT for APKTool (Android Asset Packaging Tool)"
+TERMUX_SUBPKG_DEPENDS="libc++, libexpat, libpng, libzopfli, zlib"
